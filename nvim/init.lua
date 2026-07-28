9a574f2e6b5317f4223712b772e43f714bdefb05
Ahmad Dzuizz Annajib <ahmad.dzuizz.annajib@gmail.com>
2026-07-28 17:14:54 +0800

    minimal nvim setup as prep for io vim environment

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,27 @@
-require("config.lazy")
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.keymap.set("n", "<Esc>", "<cmd>noh<CR>", { desc = "Clear search highlight" })
+
+local jinja_plugin = "/opt/homebrew/lib/node_modules/prettier-plugin-jinja-template/lib/index.js"
+vim.opt.autoread = true
+vim.api.nvim_create_autocmd("BufWritePost", {
+  pattern = { "*.html", "*.css", "*.js", "*.ts", "*.json", "*.md", "*.yaml", "*.yml", "*.jinja", "*.j2" },
+  callback = function(a)
+    if vim.fn.executable("prettier") ~= 1 then return end
+    local cmd = { "prettier", "--write", a.file }
+    if a.file:match("%.jinja$") or a.file:match("%.j2$") then
+      cmd = { "prettier", "--plugin=" .. jinja_plugin, "--parser=jinja-template", "--write", a.file }
+    end
+    vim.system(cmd, {}, function()
+      vim.schedule(function() vim.cmd("checktime") end)
+    end)
+  end,
+})
--- nvim/lua/config/autocmds.lua
+++
@@ -1,6 +0,0 @@
--- Highlight yanked text (nice QoL)
-vim.api.nvim_create_autocmd("TextYankPost", {
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})
